76f4e03dcec16b20fb16b7576b4364f0477217ba
Derick Robinson <derickdev6@gmail.com>
2020-10-25 18:04:45 -0500

    Helados

--- js/master.js
+++ js/master.js
@@ -1,7 +1,27 @@
-document.write("Connected")
+document.write("Connected<br><br><br>")
 
 //Tabla de precios de helados
 
 const icprice = [0.6, 1, 1.6, 1.7, 1.8, 2.9, 2.9];
 const icname = ["Palito de agua", "Palito de crema", "Heladix", "Heladovich", "Helardo", "Confites", "Pote"];
-console.log(icname.lenght)
+
+var dinero = prompt("Cuanto dinero tienes?");
+
+var i = 0;
+var op;
+var resto;
+
+while (i < 7) {
+  console.log(icname[i] + "  -  " + icprice[i])
+
+  if (dinero < icprice[i]){
+    if (dinero >= icprice[i-1]) {
+      var op = i-1;
+    }
+  }
+
+  i++;
+}
+console.log(op);
+resto = dinero - icprice[op];
+document.write("Con los $" + dinero + " que tienes puedes comprar " + icname[op] + " que vale " + icprice[op] + " y te sobra $" + resto)
