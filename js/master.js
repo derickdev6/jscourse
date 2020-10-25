bd70a980ed60e7d5f31d55c04c210a91d4e4ed9c
Derick Robinson <derickdev6@gmail.com>
2020-10-25 17:26:22 -0500

    First commit

---
+++ js/master.js
@@ -0,0 +1,7 @@
+document.write("Connected")
+
+//Tabla de precios de helados
+
+const icprice = [0.6, 1, 1.6, 1.7, 1.8, 2.9, 2.9];
+const icname = ["Palito de agua", "Palito de crema", "Heladix", "Heladovich", "Helardo", "Confites", "Pote"];
+console.log(icname.lenght)
