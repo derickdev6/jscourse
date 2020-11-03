8facb4827f59a2ef8ed9e343798536dbc2e5a043
Derick R <derickdev6@gmail.com>
2020-11-03 00:43:43 -0500

    First function

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+document.write("The function is a saluter");
+
+function sayhello(){
+    answer = prompt("Hi, how are you?")
+
+    if (answer == "good"){
+        alert("That's good!");
+    }
+    else if (answer == "bad"){
+        alert("That's bad");
+    }
+}
+sayhello();
